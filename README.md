d415936be0ed5522403c3cfff7640d1c2adb0e7c
Mera Youssef <merayoussef91@hotmail.com>
2020-10-18 23:23:48 -0500

    final project, added function to save notes and tested app

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Note Taker Starter Code
+# Note Taker
+
+## Description
+Note Taker is an app created using Node.js Express. The app is used to write, save and delete notes (JSON file).
+
+## To run this app:
+1. Download node.js and run npm i express to run express.  
+2. In the commandline, run npm start to start the port created in the server.js file.  
+3. In your browser, type localhost:3000/ to access the main page.
+
+## Heroku link:
+https://gentle-savannah-81959.herokuapp.com/ 
+
+
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,22 +1,17 @@
-const apiRoutes = require('./routes/apiRoutes.js');
-const htmlRoutes = require('./routes/htmlRoutes.js');
-const express = require('express');
-const PORT = process.env.PORT || 3000;
+const express = require("express");
+const fs = require("fs");
+const path = require('path');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 
-// middleware
-// parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
-
-app.use('/api/notes', apiRoutes);
-app.use('*', htmlRoutes);
-// parse incoming JSON data
 app.use(express.json());
-// connected the style and javascript code in /public folder.
-app.use(express.static('public'));
+app.use(express.static(__dirname));
 
+require('./routes/routes')(app);
 
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-});
+// Setup listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});  
