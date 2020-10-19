1fec530a961199c1ca305dcb0882b4524def10c6
Mera Youssef <merayoussef91@hotmail.com>
2020-10-19 11:53:43 -0500

    added comments

--- routes/routes.js
+++ routes/routes.js
@@ -2,8 +2,6 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = app => {
-
-    // Setup notes variable
     fs.readFile("db/db.json","utf8", (err, data) => {
 
         if (err) throw err;
@@ -17,32 +15,34 @@ module.exports = app => {
         });
 
         app.post("/api/notes", function(req, res) {
+            // Receive a new note and add it to db.json
             let newNote = req.body;
             notes.push(newNote);
-            updateNoteFile();
+            updateNotesFile();
             return console.log("Added new note: "+newNote.title);
         });
 
         app.get("/api/notes/:id", function(req,res) {
+            // display json for the notes array indices of the provided id
             res.json(notes[req.params.id]);
         });
 
         app.delete("/api/notes/:id", function(req, res) {
             notes.splice(req.params.id, 1);
-            updateNoteFile();
-            console.log("Deleted note with id "+req.params.id);
+            updateNotesFile();
+            console.log("Removed note with id "+ req.params.id);
         });
 
         // HTML Routes
         app.get('/notes', function(req,res) {
-            res.sendFile(path.join(__dirname, "../public/notes"));
+            res.sendFile(path.join(__dirname, "../public/notes.html"));
         });
         
         app.get('*', function(req,res) {
-            res.sendFile(path.join(__dirname, "../public/index"));
+            res.sendFile(path.join(__dirname, "../public/index.html"));
         });
 
-        function updateNoteFile() {
+        function updateNotesFile() {
             fs.writeFile("db/db.json",JSON.stringify(notes,'\t'),err => {
                 if (err) throw err;
                 return true;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname));
