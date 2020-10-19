f68bb64ba6f3f9e9b4aca499f78533ca5a251eda
Mera Youssef <merayoussef91@hotmail.com>
2020-10-19 01:17:40 -0500

    refactored and organized code

--- routes/routes.js
+++ routes/routes.js
@@ -2,45 +2,47 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = app => {
+
+    // Setup notes variable
     fs.readFile("db/db.json","utf8", (err, data) => {
 
         if (err) throw err;
 
         var notes = JSON.parse(data);
 
+        // API Routes
         app.get("/api/notes", function(req, res) {
             // Read the db.json file 
             res.json(notes);
         });
 
         app.post("/api/notes", function(req, res) {
-            // Receive a new note and add it to db.json
             let newNote = req.body;
             notes.push(newNote);
-            updateDb();
+            updateNoteFile();
             return console.log("Added new note: "+newNote.title);
         });
 
         app.get("/api/notes/:id", function(req,res) {
-            // display json for the notes array indices of the provided id
             res.json(notes[req.params.id]);
         });
 
         app.delete("/api/notes/:id", function(req, res) {
             notes.splice(req.params.id, 1);
-            updateNotesFile();
-            console.log("Removed note with id "+ req.params.id);
+            updateNoteFile();
+            console.log("Deleted note with id "+req.params.id);
         });
 
+        // HTML Routes
         app.get('/notes', function(req,res) {
-            res.sendFile(path.join(__dirname, "../public/notes.html"));
+            res.sendFile(path.join(__dirname, "../public/notes"));
         });
         
         app.get('*', function(req,res) {
-            res.sendFile(path.join(__dirname, "../public/index.html"));
+            res.sendFile(path.join(__dirname, "../public/index"));
         });
 
-        function updateNotesFile() {
+        function updateNoteFile() {
             fs.writeFile("db/db.json",JSON.stringify(notes,'\t'),err => {
                 if (err) throw err;
                 return true;
--- server.js
+++ server.js
@@ -4,14 +4,13 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname));
 
+//Require routes file
 require('./routes/routes')(app);
 
-// Setup listener
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
 });  
