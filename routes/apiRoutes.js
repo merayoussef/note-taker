0ee33af68cd606ca8ca71f09240c66868c859fb5
Mera Youssef <merayoussef91@hotmail.com>
2020-10-21 22:07:34 -0500

    update API routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,44 +2,58 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = function(app) {
-    fs.readFile("db/db.json","utf8", (err, data) => {
-
-        if (err) throw err;
-
-        var notes = JSON.parse(data);
-
         // API Routes
         app.get("/api/notes", function(req, res) {
             // Read the db.json file 
-            res.json(notes);
+            fs.readFile("db/db.json","utf8", (err, data) => {
+
+                if (err) throw err;
+        
+                var notes = JSON.parse(data);
+                res.json(notes);
+            });
         });
 
         app.post("/api/notes", function(req, res) {
-            // Receive a new note and add it to db.json
+            fs.readFile("db/db.json","utf8", (err, data) => {
+
+                if (err) throw err;
+        
+            var notes = JSON.parse(data);
             let newNote = req.body;
-            notes.push(newNote);
-            updateNotesFile();
+            notes.push;
+            updateNotesFile(newNote);
             return console.log("Added new note: "+newNote.title);
         });
+    });
 
         app.get("/api/notes/:id", function(req,res) {
-            // display json for the notes array indices of the provided id
+        // Read the db.json file 
+        fs.readFile("db/db.json","utf8", (err, data) => {
+
+            if (err) throw err;
+    
+            var notes = JSON.parse(data);
             res.json(notes[req.params.id]);
-        });
+        }); 
+    });
 
         app.delete("/api/notes/:id", function(req, res) {
+            fs.readFile("db/db.json","utf8", (err, data) => {
+
+                if (err) throw err;
+        
+            var notes = JSON.parse(data);
             notes.splice(req.params.id, 1);
             updateNotesFile();
             console.log("Removed note with id "+ req.params.id);
         });
-
-        function updateNotesFile() {
+    });
+        function updateNotesFile(notes) {
+            console.log("notes", notes)
             fs.writeFile("db/db.json",JSON.stringify(notes,'\t'),err => {
                 if (err) throw err;
                 return true;
             });
         }
-
-    });
-
 }
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 // middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static("public"));
 
 //Require routes file
 require("./routes/apiRoutes")(app);
